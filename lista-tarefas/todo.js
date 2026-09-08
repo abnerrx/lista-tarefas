2f1f1bb86711480eeea710c86d4349ee125dbde9
Abner Rodrigues Rolim <abnerrodriguesrolim@gmail.com>
2026-09-08 17:20:49 -0300

    Alterei algumas coisas do código, adicionei um botão de deletar para cada tarefa própria e adicionei um botão para alterar tarefa com base no ID, e a mudança da cor de fundo é só pra questões de conforto

--- lista-tarefas/todo.js
+++ lista-tarefas/todo.js
@@ -6,16 +6,48 @@ app.use(cors());
 app.use(express.json());
 
 let tarefas = [];
+let contador = 0;
 
-app.post('/tarefas', (req, res) =>{
-    let tarefaNova = req.body;
-    tarefas.push(tarefaNova)
+app.post('/tarefas', (req, res) => {
+    let tarefaNova = {
+        id: contador,
+        titulo: req.body.titulo
+    };
+    contador++;
+    tarefas.push(tarefaNova)    
     res.send("Tarefa adicionada!")
+});
+
+app.put('/tarefas/:id', (req, res) => {
+    let id = Number(req.params.id);
+    for (let i = 0; i < tarefas.length; i++){
+        if (tarefas[i].id === id){
+            tarefas[i].titulo = req.body.titulo;
+        }
+    }
+    res.send("Tarefa alterada.");
+})
+
+app.delete('/tarefas/:id', (req, res) => {
+    let nova_lista = [];
+
+    let id = Number(req.params.id);
+    for (let i = 0; i < tarefas.length; i++){
+        if (tarefas[i].id !== id){
+            nova_lista.push(tarefas[i]);
+        }
+    }
+
+    tarefas = nova_lista;
+
+    res.send("Tarefa  removida;")
+
 });
 
 app.get('/tarefas', (req, res) => {
     res.send(tarefas)
 });
+
 
 app.listen(3500, () => {
     console.log('Rodando...');
