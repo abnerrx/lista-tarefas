454d7caf537f1852dc5c0408f07a5cffc451a809
Abner Rodrigues Rolim <abnerrodriguesrolim@gmail.com>
2026-09-17 19:23:50 -0300

    antes: CRUD com array em memória
    agora:
    CRUD + PostgreSQL + variáveis de ambiente

--- lista-tarefas/todo.js
+++ lista-tarefas/todo.js
@@ -1,51 +1,59 @@
 const express = require('express');
 const cors = require('cors');
+const { Client } = require('pg');
 const app = express();
+require('dotenv').config();
+
+
+const cliente = new Client({
+    user: 'postgres',
+    password: process.env.DB_PASSWORD,
+    host: 'localhost',
+    database: 'lista_tarefas',
+    port: 5432
+});
+
+cliente.connect();
 
 app.use(cors());
 app.use(express.json());
 
-let tarefas = [];
-let contador = 0;
 
-app.post('/tarefas', (req, res) => {
-    let tarefaNova = {
-        id: contador,
-        titulo: req.body.titulo
-    };
-    contador++;
-    tarefas.push(tarefaNova)    
-    res.send("Tarefa adicionada!")
+
+
+app.post('/tarefas', async (req, res) => {
+    await cliente.query(
+        'INSERT INTO tarefas (titulo) VALUES ($1)',
+        [req.body.titulo]
+    );
+    res.send("Tarefa Adicionada.")
 });
 
-app.put('/tarefas/:id', (req, res) => {
-    let id = Number(req.params.id);
-    for (let i = 0; i < tarefas.length; i++){
-        if (tarefas[i].id === id){
-            tarefas[i].titulo = req.body.titulo;
-        }
-    }
-    res.send("Tarefa alterada.");
-})
-
-app.delete('/tarefas/:id', (req, res) => {
-    let nova_lista = [];
-
-    let id = Number(req.params.id);
-    for (let i = 0; i < tarefas.length; i++){
-        if (tarefas[i].id !== id){
-            nova_lista.push(tarefas[i]);
-        }
-    }
-
-    tarefas = nova_lista;
-
-    res.send("Tarefa  removida;")
-
+app.put('/tarefas/:id', async (req, res) => {
+    const id = Number(req.params.id);
+    
+    await cliente.query(
+        'UPDATE tarefas SET titulo = $1 WHERE id = $2',
+        [req.body.titulo, id]
+    );
+    res.send("Tarefa alterada.")
 });
 
-app.get('/tarefas', (req, res) => {
-    res.send(tarefas)
+
+app.delete('/tarefas/:id', async (req, res) => {
+    const id = Number(req.params.id);
+
+    await cliente.query(
+        'DELETE FROM tarefas WHERE id = $1',
+        [id]
+    );
+    
+    res.send("Tarefa  removida.")
+});
+
+app.get('/tarefas', async (req, res) => {
+    const resultado = await cliente.query('SELECT * FROM tarefas');
+    res.send(resultado.rows);
 });
 
 
